2e3319bdd3bd766841dbf4a0796784776c2afc09
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2021-09-13 17:15:25 +0200

    feat: initial commit

---
+++ tests/DetectorTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use Homoglyph\Detector;
+use PHPUnit\Framework\TestCase;
+
+class DetectorTest extends TestCase
+{
+    /**
+     * @dataProvider provider
+     */
+    public function test(int $expectedCount, string $inputText, array $targetWords): void
+    {
+        $d = new Detector();
+        $matches = $d->search($inputText, $targetWords);
+        self::assertCount($expectedCount, $matches);
+    }
+
+    public function provider(): Generator
+    {
+        yield 'simple' => [
+            1,
+            '𝐍𝐞𝐭𝐉𝐨𝐛𝟏.𝐜𝐨𝐦',
+            ['netjob1.com']
+        ];
+
+        yield 'multiple banned words' => [
+            2,
+            '𝐍𝐞𝐭𝐉𝐨𝐛𝟏.𝐜𝐨𝐦 and abc',
+            ['netjob1.com', 'abc']
+        ];
+
+        yield 'soft hyphens' => [
+            1,
+            'no NOT join 𝐍­𝐞­𝐭­𝐉­𝐨­𝐛­𝟏­.𝐜­𝐨­𝐦',
+            ['netjob1.com']
+        ];
+
+        yield 'multiple occurrences' => [
+            2,
+            'do NOT join netjob1.com - netjob1.com is a bad site',
+            ['netjob1.com']
+        ];
+
+        yield 'no banned word contained' => [
+            0,
+            'lorem ipsum',
+            ['google.com']
+        ];
+    }
+}
